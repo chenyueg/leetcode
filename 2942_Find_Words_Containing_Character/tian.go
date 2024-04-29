e17addc66329a74162b66fc374049dade8178f71
Violet Feng <violetzijing@gmail.com>
2024-04-29 13:18:30 -0700

    tian: Add 2942 solution

---
+++ 2942_Find_Words_Containing_Character/tian.go
@@ -0,0 +1,11 @@
+import "strings"
+
+func findWordsContaining(words []string, x byte) []int {
+	result := []int{}
+	for i, w := range words {
+		if strings.ContainsRune(w, rune(x)) {
+			result = append(result, i)
+		}
+	}
+	return result
+}
