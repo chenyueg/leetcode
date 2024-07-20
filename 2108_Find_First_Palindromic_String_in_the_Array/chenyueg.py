39614b90c15120096090360ad02586a5abc059db
Yue Chen <chenyueg@gmail.com>
2024-07-20 11:25:32 -0700

    added problem 2108

---
+++ 2108_Find_First_Palindromic_String_in_the_Array/chenyueg.py
@@ -0,0 +1,6 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for each in words:
+            if each == each [::-1]:
+                return each
+        return ""
