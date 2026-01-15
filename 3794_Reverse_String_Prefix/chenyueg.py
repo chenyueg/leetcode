64a07d2d434cca37e3b74da1bcdbe1aacfae1a90
Yue Chen <chenyue@microsoft.com>
2026-01-15 10:35:34 -0800

    added problem 3794

---
+++ 3794_Reverse_String_Prefix/chenyueg.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reversePrefix(self, s: str, k: int) -> str:
+        s = list(s)
+        
+        for i in range(k // 2):
+            s[i], s[k - 1 - i] = s[k - 1 - i], s[i]
+        
+        return "".join(s)
